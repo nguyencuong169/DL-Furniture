2ab272b8bd504ecb849e09a88cd7a4532a229af8
Nguyen Hung Cuong <Nguyen.Hung.Cuong@usol-v.com.vn>
2024-02-11 00:44:44 +0700

    update

--- src/template/02_SliderComponent.vue
+++ src/template/02_SliderComponent.vue
@@ -13,7 +13,7 @@ const state = reactive({
       <div
         class="text-center item bg-img"
         :data-overlay-dark="state.overlayDark"
-        data-background="https://dl-furniture.netlify.app/assets/img/slider/2.jpg"
+        data-background="https://dl-furniture.netlify.app/assets/2-xOjvuLYK.jpg"
       >
         <img src="../assets/img/slider/2.jpg" v-show="false" />
         <div class="v-middle caption">
@@ -43,7 +43,7 @@ const state = reactive({
       <div
         class="text-center item bg-img"
         :data-overlay-dark="state.overlayDark"
-        data-background="https://dl-furniture.netlify.app/assets/img/slider/3.jpg"
+        data-background="https://dl-furniture.netlify.app/assets/3-DTrcElLx.jpg"
       >
         <img src="../assets/img/slider/3.jpg" v-show="false" />
         <div class="v-middle caption">
@@ -70,7 +70,7 @@ const state = reactive({
       <div
         class="text-center item bg-img"
         :data-overlay-dark="state.overlayDark"
-        data-background="https://dl-furniture.netlify.app/assets/img/slider/1.jpg"
+        data-background="https://dl-furniture.netlify.app/assets/1-FQ5yJaaV.jpg"
       >
         <img src="../assets/img/slider/1.jpg" v-show="false" />
         <div class="v-middle caption">
