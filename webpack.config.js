5819e65dcf48fd81c506efb5aa0101aee6ebe836
Nguyen Hung Cuong <Nguyen.Hung.Cuong@usol-v.com.vn>
2024-02-13 22:23:43 +0700

    up

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     //     Vue: ['vue/dist/vue.esm.js', 'default']
     //   })
     new webpack.ProvidePlugin({
+      Vue: ['vue/dist/vue.esm.js', 'default'],
       $: 'jquery',
       jQuery: 'jquery',
       jquery: 'jquery',
